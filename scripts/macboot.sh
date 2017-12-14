829c9113bc9c2adf22fc66e6534dd014a29edabe
kklipsch <junk@klipsch.net>
2017-12-14 09:49:02 -0600

    remove macboot script as it never worked.

--- scripts/macboot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cat <<'DONE'
-gpgconf /kill gpg-agent && $(gpg-agent --enable-ssh-support --daemon)
-devenv(){ docker run -ti -v ${PWD}:/home/kklipsch/$(basename $PWD) kklipsch/devenv:$1; }
-DONE
