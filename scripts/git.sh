c098434732a6005fd70e3de9d4f7db1c85a8d7ec
kklipsch <kklipsch@example.com>
2017-12-23 16:37:20 +0000

    install hub

--- scripts/git.sh
+++ scripts/git.sh
@@ -7,3 +7,8 @@ git config --global user.email "kklipsch@example.com"
 git config --global core.editor vi
 git config --global alias.stat status
 
+
+curl -sL https://github.com/github/hub/releases/download/v2.2.8/hub-linux-amd64-2.2.8.tgz | tar zx -C /tmp
+/tmp/hub-linux-amd64-2.2.8/install 
+cp /tmp/hub-linux-amd64-2.2.8/etc/* /usr/local/etc
+rm /tmp/hub-linux-amd64-2.2.8/install 
