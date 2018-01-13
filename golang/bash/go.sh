12e0152cb383f4c11bc4c8d32a954c956bed7db4
kklipsch <kklipsch@example.com>
2018-01-13 14:23:29 +0000

    have ag ignore vendor files in golang image

--- golang/bash/go.sh
+++ golang/bash/go.sh
@@ -6,3 +6,6 @@ export PATH=$GOBIN:$GOROOT/bin:$PATH
 
 #first go path is used for go-get things to segregate them from my code
 export GOPATH=$HOME/src:$HOME/projects/go
+
+#dont search vendor files in golang projects
+alias ag='ag --ignore vendor'
