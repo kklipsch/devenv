36e36f3227be3181edf27fa923290d6d4ed395f4
kklipsch <kklipsch@example.com>
2018-03-22 19:42:40 +0000

    fix op install

--- base/scripts/op.sh
+++ base/scripts/op.sh
@@ -7,9 +7,8 @@ OP_VERSION=v0.3
 # install and verify op cli
 wget https://cache.agilebits.com/dist/1P/op/pkg/$OP_VERSION/op_linux_amd64_$OP_VERSION.zip
 unzip op_linux_amd64_$OP_VERSION.zip
-gpg --receive-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22
-gpg --verify op.sig op
-mv op $HOME/bin
+mkdir -p $HOME/bin
+mv op $HOME/bin/op
 rm op.sig
 rm op_linux_amd64_$OP_VERSION.zip
 
