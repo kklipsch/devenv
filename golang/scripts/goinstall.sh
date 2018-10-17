9a720f2ed1f75ee07fb145820a3167c0ebc49ccc
kklipsch <kklipsch@example.com>
2018-10-17 18:22:48 +0000

    upgrade to go 1.11

--- golang/scripts/goinstall.sh
+++ golang/scripts/goinstall.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-VERSION=${1:-1.9.2}
+VERSION=${1:-1.11}
 
 curl -sL https://storage.googleapis.com/golang/go$VERSION.linux-amd64.tar.gz | tar zx -C /usr/local
 curl -sL https://github.com/fatih/vim-go/archive/v1.15.tar.gz | tar zx -C $VIMPACKAGES/start 
