3d999e0fdcc23767ef8f5b8c6b6a266bf09294c7
kklipsch <junk@klipsch.net>
2019-02-14 12:27:01 -0600

    Update hub to 2.9.0

--- base/scripts/git.sh
+++ base/scripts/git.sh
@@ -7,11 +7,11 @@ git config --global user.email "kklipsch@example.com"
 git config --global core.editor vi
 git config --global alias.stat status
 
-
-curl -sL https://github.com/github/hub/releases/download/v2.2.8/hub-linux-amd64-2.2.8.tgz | tar zx -C /tmp
-/tmp/hub-linux-amd64-2.2.8/install 
-cp /tmp/hub-linux-amd64-2.2.8/etc/* /usr/local/etc
-rm /tmp/hub-linux-amd64-2.2.8/install 
+hub_version=2.9.0
+curl -sL https://github.com/github/hub/releases/download/v$hub_version/hub-linux-amd64-$hub_version.tgz | tar zx -C /tmp
+/tmp/hub-linux-amd64-$hub_version/install 
+cp /tmp/hub-linux-amd64-$hub_version/etc/* /usr/local/etc
+rm /tmp/hub-linux-amd64-$hub_version/install 
 
 #hub store things in ~/.config put that in the data directory for safekeeping
 ln -s /root/data/config $HOME/.config
