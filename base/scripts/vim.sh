599622cfdefca48db83c2d56a29c6d23999831a3
kklipsch <junk@klipsch.net>
2019-03-08 13:00:57 -0600

    Add yaml-vim plugin

--- base/scripts/vim.sh
+++ base/scripts/vim.sh
@@ -9,3 +9,4 @@ curl -sL https://github.com/tpope/vim-sensible/archive/v1.1.tar.gz | tar zx -C $
 curl -sL https://github.com/kklipsch/vim-colorschemes/archive/v1.0.tar.gz | tar zx -C $VIMPACKAGES/start
 curl -sL https://github.com/vim-syntastic/syntastic/archive/3.8.0.tar.gz | tar zx -C $VIMPACKAGES/start
 curl -sL https://github.com/kklipsch/docker-vim/archive/v1.0.tar.gz | tar zx -C $VIMPACKAGES/start
+curl -sL https://github.com/mrk21/yaml-vim/archive/v0.1.0.tar.gz | tar zx -C $VIMPACKAGES/start
